367ba1065bcc39f92d002ab7773f39446902919c
asantibanez <santibanez.andres@gmail.com>
2016-10-26 15:38:43 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# vue-router-example
+
+> A Vue.js project that uses Vue-Router for view routing. It renders Vue components using both 
+Render functions and Template strgins
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+```
---
+++ src/main.js
@@ -0,0 +1,40 @@
+
+import Vue from 'vue/dist/vue'
+import VueRouter from 'vue-router'
+import App from './App.vue'
+
+Vue.use(VueRouter);
+
+const RenderedByFunctionsComponent = {
+    render: h => h('div', {id: 'element'}, [
+        h('h1', {id: 'hi'}, 'Hello'),
+        h('h1', 'Bye')
+    ])
+};
+
+const TemplateRenderedCompoment = {
+    template: `
+        <div>Dashboard</div>
+    `
+};
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        { path: '/', component: App},
+        { path: '/hyperscript', component: RenderedByFunctionsComponent},
+        { path: '/template', component: TemplateRenderedCompoment}
+    ]
+});
+
+new Vue({
+    router,
+    template: `
+        <div id="app">
+            <router-link to="/">Home</router-link>
+            <router-link to="/hyperscript">Hyperscript</router-link>
+            <router-link to="/template">Template</router-link>
+            <router-view></router-view>
+        </div>
+    `
+}).$mount('#app');
